db1a9eac89f2d3cc86c3f2f8f150b7991fc58306
Krzysztof <krzubrycki@gmail.com>
2022-02-25 20:49:00 +0100

    Created calculator implementation with POST mapping

--- src/main/java/com/javastart/services/CalculatorService.java
+++ src/main/java/com/javastart/services/CalculatorService.java
@@ -1,4 +1,13 @@
 package com.javastart.services;
 
 public class CalculatorService {
+    public static double calculate(double a, double b, String operator) {
+        return switch (operator) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> a / b;
+            default -> throw new IllegalArgumentException("Nieobsługiwalny operator matematyczny");
+        };
+    }
 }
